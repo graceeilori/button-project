f4a98deb2d7265b8d6ee912dd2b8bdfdab187e17
Christian <you@example.com>
2026-01-29 17:10:19 -0700

    All images displaying

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,24 +5,78 @@ export default function SolarSystem() {
     <div className="solar-system">
       <div className="sun">☀</div>
 
-      <Planet className="planet planet-top" />
-      <Planet className="planet planet-right" />
-      <Planet className="planet planet-bottom" />
-      <Planet className="planet planet-left" />
+      <Planet
+       className="planet planet-top" 
+       planetImg="/caleb_icons/calebMotorcyclePlanet.png"
+       moons={[
+          "/caleb_icons/hiking.png",
+          "/caleb_icons/piano.png",
+          "/caleb_icons/snowboarding.png",
+          "/caleb_icons/terminator.png",
+        ]}
+      />
+      <Planet 
+        className="planet planet-right" 
+        planetImg="/chris_icons/chris_icon.png"
+        moons={[
+          "/chris_icons/catalog-magazine.png",
+          "/chris_icons/gun-shooting.svg",
+          "/chris_icons/heart.svg",
+          "/chris_icons/snowboarding.svg",
+        ]}
+        />
+      <Planet 
+        className="planet planet-bottom" 
+        planetImg="/grace_icons/Planet.png"
+        moons={[
+          "/grace_icons/oven.png",
+          "/grace_icons/gun.png",
+          "/grace_icons/scd.png",
+          "/grace_icons/piano.png",
+        ]}
+        />
+      <Planet 
+        className="planet planet-left" 
+        planetImg="/jj_icons/JJ_Cards_581.png"
+         moons={[
+          "/jj_icons/transgender_flag.png",
+          "/jj_icons/hiking.png",
+          "/jj_icons/oven.png",
+          "/jj_icons/hearts.png",
+        ]}
+        />
     </div>
   );
 }
 
-function Planet({ className }: { className: string }) {
+type PlanetProps = {
+  className: string;
+  planetImg: string;
+  moons: [string, string, string, string];
+};
+
+function Planet({ className, planetImg, moons }: PlanetProps) {
   return (
     <div className={className}>
-      <div className="planet-core" />
+      {/* Planet core */}
+      <div className="planet-core circle">
+        <img src={planetImg} alt="Planet" />
+      </div>
 
+       {/* Moons */}
       <div className="moons">
-        <div className="moon moon-0" />
-        <div className="moon moon-1" />
-        <div className="moon moon-2" />
-        <div className="moon moon-3" />
+        <div className="moon moon-0 circle">
+          <img src={moons[0]} alt="" />
+        </div>
+        <div className="moon moon-1 circle">
+          <img src={moons[1]} alt="" />
+        </div>
+        <div className="moon moon-2 circle">
+          <img src={moons[2]} alt="" />
+        </div>
+        <div className="moon moon-3 circle">
+          <img src={moons[3]} alt="" />
+        </div>
       </div>
     </div>
   );
